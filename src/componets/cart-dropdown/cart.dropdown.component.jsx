f69b2c19d7c4281c0c0371728651f139d274fd33
Aditya Pandey <adityapandey107@gmail.com>
2022-08-24 00:21:22 +0530

    Create Cart CheckOut and Other Functionalities

--- src/componets/cart-dropdown/cart.dropdown.component.jsx
+++ src/componets/cart-dropdown/cart.dropdown.component.jsx
@@ -1,12 +1,24 @@
 import './cart-dropdown.styles.scss';
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import {useContext} from "react";
+import {CartContext} from "../../contexts/cart.context";
+import {useNavigate} from "react-router-dom";
 
-const CartDropdown = () =>
-{
+const CartDropdown = () =>{
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckOutHandler = () => {
+        navigate('/checkout');
+    }
+
     return(
         <div className='cart-dropdown-container'>
-            <div className='cart-items' />
-            <Button>CheckOut</Button>
+            <div className='cart-items'>
+                {cartItems.map(item => <CartItem key={item.key} cartItem={item} />)}
+            </div>
+            <Button onClick={goToCheckOutHandler}>CheckOut</Button>
         </div>
     )
 }
